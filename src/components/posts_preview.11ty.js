b6d395fd4eb933062c65ee4bf36b37d114fcaa10
sofia_ost <sofia.ost4@gmail.com>
2022-02-07 13:48:52 +0200

    fix: style for blog page

--- src/components/posts_preview.11ty.js
+++ src/components/posts_preview.11ty.js
@@ -1,22 +1,28 @@
 module.exports = async function (data) {
   return /* html */ `
     <div class="container">
+      <h1 class="title-1 posts-title">The Blog</h1>
       <div class="posts-list">
         ${(
           await Promise.all(
-            data.map(async ({ title, image }) => {
+            data.map(async ({ title, image, date }) => {
               const postLink = title.replace(/ /g, "-");
+              const newDate = new Date(date);
+              const year = newDate.getFullYear();
+              const month = newDate.toLocaleString("en-US", { month: "long" });
+              const day = ("0" + newDate.getDate()).slice(-2);
 
               return /* html */ `
               <div class="blog/post-card">
-                <a href="${postLink}">
+                <a href="blog/${postLink}" class="post-image-wrapper">
                   ${await this.image(image, {
                     alt: "post preview image",
                     classes: "post-image",
                   })}
                 </a>
+                <span class="date">${month} ${day}, ${year}</span>
                 <a href="blog/${postLink}" class="post-link">
-                  <h2 class="title-2 post-card-title">${title}</h2>
+                  ${title}
                 </a>
               </div>
           `;
